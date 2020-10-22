2aed887d9c96504eaa454db38e52127a54252131
Sebastian Halkjær Petersen <seba449h@edu.easj.dk>
2020-10-22 11:46:12 +0200

    Oprettet FileEndPoint Oprettet Filescontroller -Oprettet dictionary med elementer -Oprettet metoder til dictionary

---
+++ P2PRestService/Controllers/FilesController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P2PRestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private static Dictionary<string , List<FileEndPoint>> dic = new Dictionary<string, List<FileEndPoint>>();
+
+        private string filename1 = "Test.txt";
+        FileEndPoint fep1 = new FileEndPoint("localhost", 3300);
+        FileEndPoint fep2 = new FileEndPoint("localhost", 4000);
+
+
+        private static void InsertIntoDictionary(Dictionary<string, List<FileEndPoint>> dic, string filename, FileEndPoint fep)
+        {
+            if (dic.ContainsKey(filename))
+            {
+                List<FileEndPoint> ListOfFeps = dic[filename];
+                ListOfFeps.Add(fep);
+            }
+            else
+            {
+                List<FileEndPoint> ListOfFeps = new List<FileEndPoint>();
+                ListOfFeps.Add(fep);
+                dic.Add(filename, ListOfFeps);
+            }
+        }
+
+        private static List<FileEndPoint> SearchInDictionary(Dictionary<string, List<FileEndPoint>> dic, string filename)
+        {
+            if (dic.ContainsKey(filename))
+            {
+                return dic[filename];
+            }
+
+            return new List<FileEndPoint>();
+        }
+
+        // GET: api/Files
+        [HttpGet]
+        public IEnumerable<FileEndPoint> Get(string filename)
+        {
+            return dic[filename];
+        }
+
+        // GET: api/Files/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Files
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Files/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ P2PRestService/FileEndPoint.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2PRestService
+{
+    public class FileEndPoint
+    {
+		private string _ip_adress;
+
+		public string Ip_adress
+		{
+			get { return _ip_adress; }
+			set { _ip_adress = value; }
+		}
+
+		private int _Portnr;
+
+		public int PortNr
+		{
+			get { return _Portnr; }
+			set { _Portnr = value; }
+		}
+
+        public FileEndPoint(string ipAdress, int portnr)
+        {
+            _ip_adress = ipAdress;
+            _Portnr = portnr;
+        }
+
+        public FileEndPoint()
+        {
+            
+        }
+
+        
+
+	}
+}
